423097aa5680d7e53232eb9cbe6782de254e5108
Alfredo Altamirano <fredy.altamirano8@gmail.com>
2014-07-04 01:42:06 -0700

    Initial commit

---
+++ build/src/run.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    
+    string currentDir(argv[0]);
+    currentDir = currentDir.substr(0, currentDir.find_last_of('/') + 1);
+    
+    string srcFile = currentDir + "../src/problem.cpp";
+    string inputFile = currentDir + "../src/input.txt";
+    
+	ifstream source(srcFile.c_str());
+	int i = 0;
+	string line;
+	getline(source, line);
+	bool created = false;
+	if(!line.find("/*")) {
+		ofstream input(inputFile.c_str());
+		created = true;
+		while(getline(source, line) && line.find("*/"))
+			input << line << endl;
+		input.close();
+	}
+	
+	source.close();
+
+	bool syntaxError = system(("g++ -DDEBUG -o problem " + srcFile).c_str());
+	if(!syntaxError) {
+        ifstream input(inputFile.c_str());
+		if(input.good()) 
+			system(("./problem < " + inputFile).c_str());
+		else
+			system("./problem");
+
+		system("rm problem");
+		input.close();
+		
+		if (created)
+			system(("rm " + inputFile).c_str());
+	}
+}
