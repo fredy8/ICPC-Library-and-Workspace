4398c51c420d7528e7fc69357bb0eeacb86b582b
Alfredo Altamirano <fredy.altamirano8@gmail.com>
2014-07-06 21:06:58 -0700

    Implemented basic functionality for replacing includes in the source file with the actual file. Recursive include not yet implemented.

--- build/src/include.cpp
+++ build/src/include.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include <fstream>
+#include <regex>
 
 using namespace std;
 
-int main() {
+bool isInclude(string line) {
+    string pattern = "^\\s*#\\s*include\\s*\"(.+)\"\\s*$";
+    
+    return regex_match(line.c_str(), regex(pattern.c_str()));
+}
 
+string getIncludedFile(string includeLine) {
+    int initialPos = 0, finalPos = includeLine.length() - 1;
+    while(includeLine[initialPos++] != '"');
+    while(includeLine[finalPos--] != '"');
+    
+    return includeLine.substr(initialPos, finalPos - initialPos + 1);
+}
+
+int main() {
+    ifstream source("../src/problem.cpp");
+    ofstream temp("../src/temp.cpp");
+    
+    string line;
+    while(getline(source, line))
+    {
+        if(isInclude(line)) {
+            temp << ("//" + line) << endl;
+            ifstream includeFile("../../lib/src/" + getIncludedFile(line));
+            while(getline(includeFile, line)) {
+                temp << line << endl;
+            }
+            includeFile.close();
+        } else {
+            temp << line << endl;
+        }
+    }
+    
+    source.close();
+    temp.close();
+    
+    system("mv ../src/temp.cpp ../src/problem.cpp");
 }
